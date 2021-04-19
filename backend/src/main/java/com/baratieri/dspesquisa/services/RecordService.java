762745e56d62c770f5b479d54c62ee80ac0c1858
TiagoBaratieri <tiagobaratieri97@gmail.com>
2021-04-19 17:07:39 -0300

    Post Record

---
+++ backend/src/main/java/com/baratieri/dspesquisa/services/RecordService.java
@@ -0,0 +1,39 @@
+package com.baratieri.dspesquisa.services;
+
+import com.baratieri.dspesquisa.dto.RecordDTO;
+import com.baratieri.dspesquisa.dto.RecordInsertDTO;
+import com.baratieri.dspesquisa.entities.Game;
+import com.baratieri.dspesquisa.entities.Record;
+import com.baratieri.dspesquisa.repositories.GameRepository;
+import com.baratieri.dspesquisa.repositories.RecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+
+@Service
+public class RecordService {
+
+    @Autowired
+    RecordRepository repository;
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Transactional
+    public RecordDTO insert(RecordInsertDTO dto){
+        Record entity = new Record();
+        entity.setName(dto.getName());
+        entity.setAge(dto.getAge());
+        entity.setMoment(Instant.now());
+
+        Game game = gameRepository.getOne(dto.getGameId());
+        entity.setGame(game);
+
+        entity = repository.save(entity);
+        return new RecordDTO(entity);
+    }
+
+
+}
